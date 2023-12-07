87ae5127f8c607ac103cfc9268c4314930712fc5
kheiyoshida <kheiyoshida@gmail.com>
2023-12-07 19:19:37 +0900

    centralized jest

---
+++ jest.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from 'jest'
+
+const RootConfig:Config = {
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1'
+  },
+  testEnvironment: 'jsdom',  
+  testMatch: [
+    '<rootDir>/src/**/*.test.ts'
+  ], 
+}
+
+const JestConfig:Config = {
+  projects: [
+    // pjs
+    {
+      ...RootConfig,
+      displayName: 'maze',
+      rootDir: './pjs/maze',
+    },
+    {
+      ...RootConfig,
+      displayName: 'sketch',
+      rootDir: './pjs/sketch',
+    },
+    // pkgs
+    {
+      ...RootConfig,
+      displayName: 'mgnr',
+      rootDir: './pkgs/mgnr',
+      coverageDirectory: '<rootDir>/src',
+      collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    }
+  ]
+}
+export default JestConfig
