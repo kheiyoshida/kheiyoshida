33a7a24f7cd92165adee6a7b0adbc625d2525679
kheiyoshida <kheiyoshida@gmail.com>
2023-12-10 20:58:30 +0900

    coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -11,10 +11,27 @@ const RootConfig: Config = {
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.ts'],
   prettierPath: require.resolve('prettier-2'),
-  setupFiles: ['../../jest/globalSetup.ts']
+  setupFiles: ['../../jest/globalSetup.ts'],
 }
 
 const JestConfig: Config = {
+  // even thoug it's on top level, this is for each project level
+  // do not run --coverage from root dir
+  collectCoverageFrom: [
+    '<rootDir>/**/*.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/*.config.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 60,
+      branches: 80,
+      functions: 95,
+      lines: 60,
+    },
+  },
+  coverageDirectory: 'jest/coverage',
   projects: [
     // pjs
     {
@@ -32,15 +49,11 @@ const JestConfig: Config = {
       ...RootConfig,
       displayName: 'mgnr',
       rootDir: './pkgs/mgnr',
-      coverageDirectory: '<rootDir>/src',
-      collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
     },
     {
       ...RootConfig,
       displayName: 'utils',
       rootDir: './pkgs/utils',
-      coverageDirectory: '<rootDir>/src',
-      collectCoverageFrom: ['<rootDir>/src/**/*.ts'],      
     },
   ],
 }
---
+++ pkgs/utils/src/store/store.test.ts
@@ -0,0 +1,7 @@
+import { makeStore } from "./store";
+
+describe(`${makeStore.name}`, () => {
+  it.todo(`can init with initialValue`)
+  it.todo(`can update a single field`)
+  it.todo(`can update multiple fields`)
+})
