d5c21744b347792ee72561e777f2ac6a4df0de8f
kheiyoshida <kheiyoshida@gmail.com>
2023-12-11 13:31:55 +0900

    percent

--- pkgs/utils/src/calc/percentage.ts
+++ pkgs/utils/src/calc/percentage.ts
@@ -1,9 +1,4 @@
-/**
- * convert percent in float number into float with 2 numbers after precision
- */
-export const roundPercent = (n: number) => Math.round(n * 100) / 100
 
-/**
- * convert percent in int into float number
- */
-export const toFloatPercent = (n: number) => n * 0.01
+export const roundFloatPercent = (n: number) => Math.round(n * 100) / 100
+
+export const toFloatPercent = (percentInInt: number) => roundFloatPercent(percentInInt * 0.01)
