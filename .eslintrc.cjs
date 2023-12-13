eb04730e95cf54e6b51d718d8e87886fcc869232
kheiyoshida <kheiyoshida@gmail.com>
2023-12-13 18:45:14 +0900

    any

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,5 +23,6 @@ module.exports = {
       "@typescript-eslint/no-non-null-assertion": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "no-console": "warn",
+      "@typescript-eslint/no-explicit-any": "warn"
     }
 }
