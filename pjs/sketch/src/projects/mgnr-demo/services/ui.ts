7d2586a478d6901f1a5f50158b43b3bafbe51fec
kheiyoshida <kheiyoshida@gmail.com>
2024-03-04 12:03:30 +0900

    update instruction

--- pjs/sketch/src/projects/mgnr-demo/services/ui.ts
+++ pjs/sketch/src/projects/mgnr-demo/services/ui.ts
@@ -2,10 +2,10 @@ import { MOBILE_WIDTH } from '../constants'
 
 export const showInstruction = (onConfirm: () => void) => {
   const div = p.createDiv()
-  div.style('text-align', 'center')
   div.style('color', p.color(200))
   div.style('width', '100vw')
   div.style('height', '100vh')
+  div.style('padding', '1rem')
   div.position(0, 0)
   div.style('background-color', p.color(0, 0, 0, 100))
   div.mouseClicked(() => {
@@ -15,16 +15,26 @@ export const showInstruction = (onConfirm: () => void) => {
   div.style('line-height', '1.8em')
   div.style('padding-top', '30vh')
 
-  const text = p.createDiv(`[mgnr-demo]`)
-  const text2 = p.createDiv(``)
-  const text3 = p.createDiv(``)
-  const text4 = p.createDiv(
-    window.innerWidth < MOBILE_WIDTH
-      ? `CONTROL: Swipe up/down to move, swipe right/left to turn, tap to look`
-      : `CONTROL: Arrow key or WASD to move, use mouse to turn, click to look`
-  )
+  const instruction = window.innerWidth < MOBILE_WIDTH ? MobileControl : PcControl
+
+  const text = p.createDiv(`[HOW TO PLAY]`)
+  const text2 = p.createDiv(instruction[0])
+  const text3 = p.createDiv(instruction[1])
+  const text4 = p.createDiv(instruction[2])
   div.elt.appendChild(text.elt)
   div.elt.appendChild(text2.elt)
   div.elt.appendChild(text3.elt)
   div.elt.appendChild(text4.elt)
 }
+
+const MobileControl = [
+  `MOVE: Swipe up/down`,
+  'TURN: Swipe right/left',
+  'LOOK: Tap to enter Look Mode, swipe to look around, tap again to leave',
+]
+
+const PcControl = [
+  `MOVE: Press arrow keys or WASD keys`,
+  'TURN: Move cursor (mouse) from the center',
+  'LOOK: Click to enter Look Mode, move cursor to look around, click again to leave',
+]
