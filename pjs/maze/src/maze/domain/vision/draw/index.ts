93303482d93b0d169f53f358932df1c6f3955f77
kheiyoshida <kheiyoshida@gmail.com>
2024-03-07 15:28:05 +0900

    separate draw

--- pjs/maze/src/maze/domain/vision/draw/index.ts
+++ pjs/maze/src/maze/domain/vision/draw/index.ts
@@ -1,16 +1,11 @@
 import { ListenableState } from '..'
-import { allInOneDrawer, blurredDrawer, plainDrawer } from './drawers'
+import { allInOneDrawer } from './drawers'
 import { getParams } from './parameters'
 import { GridDrawer } from './types'
 
 type DrawProvider = (state: ListenableState) => GridDrawer
 
 export const normalDraw: DrawProvider = (state: ListenableState) => {
-  return allInOneDrawer(getParams(state))
-  // const params = getParams(state)
-  // return blurredDrawer(
-  //   params.visibility,
-  //   params.fog,
-  //   params.blurRate
-  // )
+  const params = getParams(state)
+  return allInOneDrawer(params)
 }
