639f970f56257249e78e5ccb237dbe3b52b8e014
kheiyoshida <kheiyoshida@gmail.com>
2024-03-09 11:47:31 +0900

    one pile events

--- pjs/maze/src/maze/service/events/commands.ts
+++ pjs/maze/src/maze/service/events/commands.ts
@@ -1,37 +1,92 @@
+import { STAND_INTERVAL_MS } from '../../config/constants'
 import * as mapper from '../../domain/maze/mapper'
-import { clearTimer } from '../timer'
-import { goDownStairs, goEvent, registerConcurrentStandEvent, openMap, turnEvent } from './events'
+import * as maze from '../../domain/maze/maze'
+import { updateStats } from '../../domain/stats'
+import { store } from '../../store'
+import {
+  renderCurrentView,
+  renderGo,
+  renderGoDownstairs,
+  renderProceedToNextFloor,
+  renderTurn,
+} from '../render'
+import { renderMap } from '../render/others/map'
+import { RenderQueue } from '../render/queue'
+import { clearTimer, setIntervalEvent } from '../timer'
 import * as validaters from './validaters'
 
 export type CommandHandler = () => void
+export type EventHandler = () => void
 
 export const go: CommandHandler = () => {
   if (!validaters.isAccepting()) return
   if (validaters.canGo()) {
-    goEvent()
+    renderGo()
+    const res = maze.navigate()
+    mapper.track(res!)
+    updateStats('walk')
+    renderCurrentView()
     if (validaters.shouldGoDownstairs()) {
-      goDownStairs()
+      store.updateAcceptCommand(false)
+      renderGoDownstairs()
+      maze.goDownStairs()
+      mapper.reset()
+      updateStats('downstairs')
+      renderProceedToNextFloor()
+      renderCurrentView()
+      RenderQueue.waitUntilQueueGetsEmpty(() => store.updateAcceptCommand(true))
     } else {
       registerConcurrentStandEvent()
     }
   }
 }
 
-const turn = (dir: 'r' | 'l') => async () => {
+const turn = (dir: 'r' | 'l') => () => {
   if (!validaters.isAccepting()) return
-  turnEvent(dir)()
+  renderTurn(dir)
+  maze.turn(dir)
+  updateStats('turn')
+  renderCurrentView()
 }
-
 export const turnRight: CommandHandler = turn('r')
 export const turnLeft: CommandHandler = turn('l')
 
-export const callMap: CommandHandler = async () => {
+export const callMap: CommandHandler = () => {
   if (!validaters.isAccepting()) return
   if (validaters.canOpenMap()) {
     clearTimer('stand')
-    openMap()
+    renderMap(mapper.query.grid, maze.query.current, maze.query.direction, maze.query.floor)
   } else {
     registerConcurrentStandEvent()
   }
   mapper.toggleMap()
+}
+
+export const initialize: EventHandler = () => {
+  maze.generateMaze()
+  mapper.reset()
+  registerConcurrentConstantEvent()
+  registerConcurrentStandEvent()
+  renderCurrentView()
+}
+
+export const registerConcurrentConstantEvent: EventHandler = () => {
+  setIntervalEvent(
+    'constant',
+    () => {
+      updateStats('constant')
+    },
+    STAND_INTERVAL_MS * 2
+  )
+}
+
+export const registerConcurrentStandEvent: EventHandler = () => {
+  setIntervalEvent(
+    'stand',
+    () => {
+      renderCurrentView()
+      updateStats('stand')
+    },
+    STAND_INTERVAL_MS
+  )
 }
