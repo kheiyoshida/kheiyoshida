86579b2a6c6d5c81fe8c31e9d69da67a881f7c56
kheiyoshida <kheiyoshida@gmail.com>
2024-06-05 13:45:40 +0900

    new grid move system

---
+++ pkgs/mgnr-tone/src/theme/grid.test.ts
@@ -0,0 +1,90 @@
+import {
+  GridPositionIndex,
+  ThemeGridPosition,
+  clampGridPositionIndex,
+  createGridPositionManager,
+  createThemeGrid,
+  translateGridPosition,
+  translateThemeAlignment,
+} from './grid'
+
+test(`${createThemeGrid.name}`, () => {
+  const grid = createThemeGrid({
+    'left-top': jest.fn(),
+    'left-middle': jest.fn(),
+    'left-bottom': jest.fn(),
+    'center-top': jest.fn(),
+    'center-middle': jest.fn(),
+    'center-bottom': jest.fn(),
+    'right-top': jest.fn(),
+    'right-middle': jest.fn(),
+    'right-bottom': jest.fn(),
+  })
+  const shift = grid.move('up')
+  expect(shift.direction).toBe('up')
+  expect(shift.theme).toBeNull()
+  expect(shift.themeAlignment).toBe('center-top')
+
+  const shift2 = grid.move('up')
+  expect(shift2.theme).not.toBeNull()
+  expect(shift2.themeAlignment).toBe('center-bottom')
+
+  const shift3 = grid.move('right')
+  expect(shift3.theme).toBeNull()
+  expect(shift3.themeAlignment).toBe('right-bottom')
+})
+
+test(`${createGridPositionManager.name}`, () => {
+  const manager = createGridPositionManager(4,4)
+  expect(manager.grid).toBe('center-middle')
+  expect(manager.theme).toBe('left-bottom')
+
+  // move within theme
+  manager.move('up')
+  expect(manager.grid).toBe('center-middle')
+  expect(manager.theme).toBe('left-middle')
+
+  // go beyond theme border
+  manager.move('left')
+  expect(manager.grid).toBe('left-middle')
+  expect(manager.theme).toBe('right-middle')
+
+  manager.move('down')
+  manager.move('down')
+  expect(manager.grid).toBe('left-bottom')
+  expect(manager.theme).toBe('right-top')
+
+  // it can't go beyond grid's edge
+  manager.move('down')
+  manager.move('down')
+  manager.move('down')
+  manager.move('down')
+  expect(manager.grid).toBe('left-bottom')
+  expect(manager.theme).toBe('right-bottom')
+})
+
+test(`${clampGridPositionIndex.name}`, () => {
+  expect(clampGridPositionIndex(0)).toBe(1)
+  expect(clampGridPositionIndex(1)).toBe(1)
+  expect(clampGridPositionIndex(9)).toBe(9)
+  expect(clampGridPositionIndex(10)).toBe(9)
+})
+
+test.each<[GridPositionIndex, GridPositionIndex, ThemeGridPosition]>([
+  [5, 5, 'center-middle'],
+  [3, 5, 'left-middle'],
+  [8, 8, 'right-top'],
+  [1, 1, 'left-bottom'],
+  [7, 9, 'right-top'],
+])(`${translateGridPosition.name} (%i, %i)`, (col, row, expected) => {
+  expect(translateGridPosition(col, row)).toBe(expected)
+})
+
+test.each<[GridPositionIndex, GridPositionIndex, ThemeGridPosition]>([
+  [1, 1, 'left-bottom'],
+  [2, 2, 'center-middle'],
+  [3, 3, 'right-top'],
+  [4, 4, 'left-bottom'],
+])(`${translateThemeAlignment.name} (%i, %i)`, (col, row, expected) => {
+  expect(translateThemeAlignment(col, row)).toBe(expected)
+})
--- pkgs/mgnr-tone/src/theme/grid.ts
+++ pkgs/mgnr-tone/src/theme/grid.ts
@@ -1,25 +1,111 @@
-import { ThemeMaker } from "./theme"
+import { IntRange, clamp } from 'utils'
+import { ThemeMaker } from './theme'
+
+export type GridPositionIndex = IntRange<1, 10>
 
 export type ThemeGrid = ReturnType<typeof createThemeGrid>
-export type ThemeGridRow = 'top' | 'center' | 'bottom'
+export type ThemeGridRow = 'top' | 'middle' | 'bottom'
 export type ThemeGridColumn = 'left' | 'center' | 'right'
-export type ThemeGridPosition = `${ThemeGridRow}-${ThemeGridColumn}`
+export type ThemeGridPosition = `${ThemeGridColumn}-${ThemeGridRow}`
+export type ThemeAlignment = `${ThemeGridColumn}-${ThemeGridRow}`
 export type ThemeGridDirection = 'up' | 'down' | 'left' | 'right'
 
-export const createThemeGrid = (themeMakers: {[position in ThemeGridPosition]: ThemeMaker}) => {
-  let lastPosition: ThemeGridPosition = 'center-center'
-  let currentPosition: ThemeGridPosition = 'center-center'
+export type ThemeShiftInfo = {
+  theme: ThemeMaker | null
+  direction: ThemeGridDirection
+  themeAlignment: ThemeAlignment
+}
+
+export const createThemeGrid = (themeMakers: { [position in ThemeGridPosition]: ThemeMaker }) => {
+  const position = createGridPositionManager()
+  let lastGridPosition: ThemeGridPosition = position.grid
   return {
-    updatePosition: (next: ThemeGridPosition) => {
-      currentPosition = next
-    },
     getInitialTheme: () => {
-      return themeMakers[lastPosition]
+      return themeMakers[lastGridPosition]
     },
-    getNextTheme: () => {
-      if (lastPosition === currentPosition) return
-      lastPosition = currentPosition
-      return themeMakers[currentPosition]
+    move: (direction: ThemeGridDirection): ThemeShiftInfo => {
+      position.move(direction)
+      if (lastGridPosition === position.grid) {
+        return {
+          theme: null,
+          direction,
+          themeAlignment: position.theme,
+        }
+      } else {
+        lastGridPosition = position.grid
+        return {
+          theme: themeMakers[position.grid],
+          direction,
+          themeAlignment: position.theme,
+        }
+      }
     },
   }
+}
+
+export const createGridPositionManager = (
+  initialCol: GridPositionIndex = 5,
+  initialRow: GridPositionIndex = 5
+) => {
+  let col: GridPositionIndex = initialCol
+  let row: GridPositionIndex = initialRow
+  return {
+    get grid(): ThemeGridPosition {
+      return translateGridPosition(col, row)
+    },
+    get theme(): ThemeGridPosition {
+      return translateThemeAlignment(col, row)
+    },
+    move: (direction: ThemeGridDirection) => {
+      if (direction === 'up') row = clampGridPositionIndex(row + 1)
+      if (direction === 'down') row = clampGridPositionIndex(row - 1)
+      if (direction === 'left') col = clampGridPositionIndex(col - 1)
+      if (direction === 'right') col = clampGridPositionIndex(col + 1)
+    },
+  }
+}
+
+export const clampGridPositionIndex = (number: number): GridPositionIndex =>
+  clamp(number, 1, 9) as GridPositionIndex
+
+export const translateGridPosition = (
+  col: GridPositionIndex,
+  row: GridPositionIndex
+): ThemeGridPosition => {
+  return `${translateGridColPosition(col)}-${translateGriwRowPosition(row)}`
+}
+
+const translateGridColPosition = (index: number): ThemeGridColumn => {
+  if (index < 4) return 'left'
+  if (index < 7) return 'center'
+  return 'right'
+}
+
+const translateGriwRowPosition = (index: number): ThemeGridRow => {
+  if (index < 4) return 'bottom'
+  if (index < 7) return 'middle'
+  return 'top'
+}
+
+export const translateThemeAlignment = (
+  col: GridPositionIndex,
+  row: GridPositionIndex
+): ThemeGridPosition => {
+  return `${translateThemeColPosition(col)}-${translateThemeRowPosition(row)}`
+}
+
+const translateThemeColPosition = (index: number): ThemeGridColumn => {
+  const i = (index - 1) % 3
+  if (i === 0) return 'left'
+  if (i === 1) return 'center'
+  if (i === 2) return 'right'
+  throw Error(`unkwon index ${index}`)
+}
+
+const translateThemeRowPosition = (index: number): ThemeGridRow => {
+  const i = (index - 1) % 3
+  if (i === 0) return 'bottom'
+  if (i === 1) return 'middle'
+  if (i === 2) return 'top'
+  throw Error(`unkwon index ${index}`)
 }
