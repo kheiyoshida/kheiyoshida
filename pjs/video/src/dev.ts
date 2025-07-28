ef40c9a1be27f21d0428fd39e05212c2387cc986
kheiyoshida <kheiyoshida@gmail.com>
2025-07-28 12:39:05 +1000

    refactor: extract screen renderer

--- pjs/video/src/dev.ts
+++ pjs/video/src/dev.ts
@@ -3,6 +3,8 @@ import fragmentSrc from './renderers/shaders/dev.frag?raw'
 import { makeVideoSupply, VideoSupply } from './media/video/supply'
 import { prepareVideoElements } from './media/video/load'
 import { videoSourceList } from './pjs/shinjuku/videos'
+import { createProgram } from './renderers/gl'
+import { ScreenRenderer } from './renderers/ScreenRenderer'
 
 let videoSupply: VideoSupply
 prepareVideoElements(videoSourceList).then((videoElements) => {
@@ -10,67 +12,7 @@ prepareVideoElements(videoSourceList).then((videoElements) => {
   videoSupply.onEnded(() => videoSupply.swapVideo())
 })
 
-const canvas = document.getElementById('canvas')! as HTMLCanvasElement
-const gl = canvas.getContext('webgl')!
-
-// Resize canvas to fill the screen
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-gl.viewport(0, 0, canvas.width, canvas.height)
-
-// Compile shader
-function compileShader(src: string, type: number) {
-  const shader = gl.createShader(type)!
-  gl.shaderSource(shader, src)
-  gl.compileShader(shader)
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    throw new Error(gl.getShaderInfoLog(shader) || undefined)
-  }
-  return shader
-}
-
-// Link program
-function createProgram(vsSrc: string, fsSrc: string) {
-  const vs = compileShader(vsSrc, gl.VERTEX_SHADER)
-  const fs = compileShader(fsSrc, gl.FRAGMENT_SHADER)
-  const program = gl.createProgram()!
-  gl.attachShader(program, vs)
-  gl.attachShader(program, fs)
-  gl.linkProgram(program)
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    throw new Error(gl.getProgramInfoLog(program) || undefined)
-  }
-  return program
-}
-
-const program = createProgram(vertexSrc, fragmentSrc)
-gl.useProgram(program)
-
-// Fullscreen quad (clip space -1 to +1)
-// video texture starts from top-left as (0,0), while WebGL starts from bottom-left (-1, -1)
-const vertices = new Float32Array([-1, -1, 0, 1, 1, -1, 1, 1, -1, 1, 0, 0, 1, 1, 1, 0])
-
-const buffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-
-const aPosition = gl.getAttribLocation(program, 'aPosition')
-const aUV = gl.getAttribLocation(program, 'aUV')
-gl.enableVertexAttribArray(aPosition)
-gl.enableVertexAttribArray(aUV)
-gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 16, 0)
-gl.vertexAttribPointer(aUV, 2, gl.FLOAT, false, 16, 8)
-
-const texture = gl.createTexture()
-gl.bindTexture(gl.TEXTURE_2D, texture)
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-
-const uTexture = gl.getUniformLocation(program, 'uTexture')
-gl.uniform1i(uTexture, 0) // texture unit 0
-
-// const video = document.getElementById('video')! as HTMLVideoElement
+const renderer = new ScreenRenderer(createProgram(vertexSrc, fragmentSrc))
 
 // render loop
 function render() {
@@ -81,11 +23,9 @@ function render() {
 
   // inject video as texture
   if (video.readyState >= video.HAVE_CURRENT_DATA) {
-    gl.bindTexture(gl.TEXTURE_2D, texture)
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video)
+    renderer.setTextureImage(video)
   }
 
-  gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+  renderer.draw()
 }
 render()
--- pjs/video/src/renderers/ScreenRenderer.ts
+++ pjs/video/src/renderers/ScreenRenderer.ts
@@ -1,1 +1,52 @@
+import { Renderer } from './Renderer'
 
+export class ScreenRenderer extends Renderer {
+  private texture!: WebGLTexture
+
+  constructor(program: WebGLProgram) {
+    super(program)
+    this.setupScreenRect()
+  }
+
+  private setupScreenRect() {
+    const gl = this.gl
+
+    // Fullscreen quad (clip space -1 to +1)
+    // video texture starts from top-left as (0,0), while WebGL starts from bottom-left (-1, -1)
+    const vertices = new Float32Array([-1, -1, 0, 1, 1, -1, 1, 1, -1, 1, 0, 0, 1, 1, 1, 0])
+
+    const buffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+
+    const aPosition = gl.getAttribLocation(this.program, 'aPosition')
+    gl.enableVertexAttribArray(aPosition)
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 16, 0)
+
+    const aUV = gl.getAttribLocation(this.program, 'aUV')
+    gl.enableVertexAttribArray(aUV)
+    gl.vertexAttribPointer(aUV, 2, gl.FLOAT, false, 16, 8) // 4bytes x2 for position
+
+    this.texture = gl.createTexture()!
+    gl.bindTexture(gl.TEXTURE_2D, this.texture)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+
+    const uTexture = gl.getUniformLocation(this.program, 'uTexture')
+    gl.uniform1i(uTexture, 0) // texture unit 0
+  }
+
+  setTextureImage(source: TexImageSource) {
+    this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture)
+    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, source)
+  }
+
+  /**
+   * draw screen rect with texture
+   */
+  draw() {
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT)
+    this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4)
+  }
+}
