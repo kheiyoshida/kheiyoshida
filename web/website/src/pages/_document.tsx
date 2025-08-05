ed921dc3c403cd79a5d337e2678dd4eb986a3a9e
kheiyoshida <kheiyoshida@gmail.com>
2025-08-05 18:14:21 +1000

    fix gtag

--- web/website/src/pages/_document.tsx
+++ web/website/src/pages/_document.tsx
@@ -1,20 +1,8 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
-
-const PUBLIC_GA_ID = process.env.NEXT_PUBLIC_GA_ID
 
 export default function Document() {
   return (
     <Html lang="en">
-
-      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GA_ID}`}></Script>
-      <Script id={'gtag'}>
-        {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${PUBLIC_GA_ID}');`}
-      </Script>
       <Head />
       <body>
         <Main />
