cc83cf51f57cbe71255dc174fffd1f7d96870260
kheiyoshida <kheiyoshida@gmail.com>
2025-08-06 16:47:51 +1000

    remove spotify link, update profile

--- web/website/src/contents/about.ts
+++ web/website/src/contents/about.ts
@@ -3,7 +3,7 @@ import { resolveImagekitPath } from '../lib/image'
 export const AboutContent = {
   photo: resolveImagekitPath('works', 'kheiyoshida', 'me4hori.jpeg'),
   introduction: [
-    `Katsumi “Khei” Yoshida is a programmer, musician, artist.`,
+    `Katsumi “Khei” Yoshida is a programmer, musician, and game developer.`,
     `He’s been working on serial experiments of music-as-software, which he believes to be the next form of music.`,
   ],
   contact: `kheiyoshida@gmail.com`,
