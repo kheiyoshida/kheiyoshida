70e14987f4de28871c5ad3a7c6707860a0d4c6cc
kheiyoshida <kheiyoshida@gmail.com>
2025-11-10 11:29:27 +1100

    feat: implement tessellation

--- pkgs/maze-models/src/pipeline/processors/tessellation.ts
+++ pkgs/maze-models/src/pipeline/processors/tessellation.ts
@@ -1,14 +1,42 @@
-import { GeometrySpec } from '../types'
+import { GeometrySpec, Vector3D } from '../types'
 
 /**
  * Adds intermediate vertices for higher mesh resolution.
  * Should respect existing vertex positions and interpolate
- * interior points (grid or random scatter).
+ * interior points randomly within the face.
  */
-export function tesselateGeometry(input: GeometrySpec): GeometrySpec {
-  // TODO: Implement per-face subdivision (grid or Delaunay scatter)
-  return {
-    ...input,
-    meta: { ...input.meta, stage: 'tessellated' },
+export const tesselateGeometry =
+  (insertionsPerFace: number) =>
+  (input: GeometrySpec): GeometrySpec => {
+    const vertices = input.vertices.slice()
+    for (const face of input.faces) {
+      const v0 = vertices[face.vertexIndices[0]]
+      const v1 = vertices[face.vertexIndices[1]]
+      const v2 = vertices[face.vertexIndices[2]]
+      for (let i = 0; i < insertionsPerFace; i++) {
+        vertices.push(randomPointOnFace(v0, v1, v2))
+        face.vertexIndices.push(vertices.length - 1)
+        face.normalIndices.push(face.normalIndices[0]) // duplicate for now
+      }
+    }
+
+    return {
+      ...input,
+      vertices,
+      meta: { ...input.meta, stage: 'tessellated' },
+    }
   }
+
+export function randomPointOnFace(v0: Vector3D, v1: Vector3D, v2: Vector3D): Vector3D {
+  let r1 = Math.random()
+  let r2 = Math.random()
+  if (r1 + r2 > 1) {
+    r1 = 1 - r1
+    r2 = 1 - r2
+  }
+  return [
+    v0[0] + r1 * (v1[0] - v0[0]) + r2 * (v2[0] - v0[0]),
+    v0[1] + r1 * (v1[1] - v0[1]) + r2 * (v2[1] - v0[1]),
+    v0[2] + r1 * (v1[2] - v0[2]) + r2 * (v2[2] - v0[2]),
+  ]
 }
