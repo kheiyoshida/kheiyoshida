8186feadf94b56d2228b2304017e37a84c52a3ab
kheiyoshida <kheiyoshida@gmail.com>
2025-11-11 17:03:37 +1100

    feat: it can provide deformed geometries for maze

--- pkgs/maze-models/src/models/index.ts
+++ pkgs/maze-models/src/models/index.ts
@@ -1,9 +1,25 @@
 import { ModelCode } from './code'
 import { GeometrySpec } from '../pipeline/types'
 import { BaseGeometryMap } from './base'
+import { runPipeline } from '../pipeline/pipeline'
+import { tesselateGeometry } from '../pipeline/processors/tessellation'
+import { triangulateFaces } from '../pipeline/processors/triangulation'
+import { deformGeometry } from '../pipeline/processors/deformation'
+import { recomputeNormals } from '../pipeline/processors/normals'
 
 export type { ModelCode } from './code'
 
 export const generateGeometry = (modelCode: ModelCode): GeometrySpec => {
-  return BaseGeometryMap[modelCode]
+  const base = BaseGeometryMap[modelCode]
+
+  return runPipeline(base, [
+    tesselateGeometry(10),
+    triangulateFaces,
+    deformGeometry((v) => [
+      v[0] + (Math.random() - 0.5) * 0.2,
+      v[1] + (Math.random() - 0.5) * 0.2,
+      v[2] + (Math.random() - 0.5) * 0.2,
+    ]),
+    recomputeNormals,
+  ])
 }
