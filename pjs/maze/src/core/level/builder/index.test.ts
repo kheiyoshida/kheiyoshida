c1b2aa629f102314c8ebaccc080a1d92b55d82ef
kheiyoshida <kheiyoshida@gmail.com>
2025-11-18 12:07:35 +1100

    feat: implement constraints for grid extra properties

--- pjs/maze/src/core/level/builder/index.test.ts
+++ pjs/maze/src/core/level/builder/index.test.ts
@@ -1,4 +1,5 @@
 import { buildMazeGrid } from './index.ts'
+import { getPositionInDirection } from '../../grid/position2d.ts'
 
 describe(`${buildMazeGrid.name}`, () => {
   it(`should generate a maze with one stair at a dead end position and one start cell with wall facing direction`, () => {
@@ -16,6 +17,25 @@ describe(`${buildMazeGrid.name}`, () => {
 
     const startCellPos = grid.findPosition((pos, item) => item !== null && item.start !== undefined)!
     expect(startCellPos).toBeDefined()
-    expect(grid.getCorridors().filter((pos) => pos.x == startCellPos.x && pos.y == startCellPos.y)).toHaveLength(1)
+    expect(
+      grid.getCorridors().filter((pos) => pos.x == startCellPos.x && pos.y == startCellPos.y)
+    ).toHaveLength(1)
+  })
+
+  it.each(Array.from({ length: 10 }))(`should generate a maze with one stair that points outside the level`, () => {
+    const grid = buildMazeGrid({
+      size: 11,
+      fillRate: 0.5,
+      connRate: 0.3,
+      stairPositionConstraint: 'horizontalExit',
+      startPositionConstraint: 'shouldFaceCorridorWall',
+    })
+
+    const stairCellPos = grid.findPosition((pos, cell) => cell !== null && cell.type === 'stair')!
+    const stairDir = grid.getDeadEndDirection(stairCellPos)
+
+    for (let i = 1; i <= 4; i++) {
+      expect(grid.get(getPositionInDirection(stairCellPos, stairDir, i))).toBeNull()
+    }
   })
 })
--- pjs/maze/src/core/level/builder/index.ts
+++ pjs/maze/src/core/level/builder/index.ts
@@ -1,8 +1,7 @@
 import { MazeGrid } from '../grid.ts'
 import { seedCells } from './seed.ts'
 import { connectCells } from './connect.ts'
-import { fireByRate, randomItemFromArray } from 'utils'
-import { getTurnedDirection } from '../../grid/direction.ts'
+import { setStairMethods, setStartMethods } from './extras.ts'
 
 const BuildRetryLimit = 20
 
@@ -17,46 +16,28 @@ export type BuildMazeGridParams = {
 export type StairPositionConstraint = 'deadEnd' | 'horizontalExit'
 export type StartPositionConstraint = 'evenPositionCellExceptStair' | 'shouldFaceCorridorWall'
 
-export const buildMazeGrid = (params: BuildMazeGridParams, retry = 0): MazeGrid => {
-  const grid = _buildMazeGrid(params)
+export class BuildMazeGridError extends Error {}
 
-  if (!isValidMazeLevel(grid)) {
+export const buildMazeGrid = (params: BuildMazeGridParams, retry = 0): MazeGrid => {
+  const { size, fillRate, connRate } = params
+  const grid = new MazeGrid(size, size)
+  seedCells(grid, fillRate)
+  connectCells(grid, connRate)
+
+  try {
+    setStairMethods[params.stairPositionConstraint](grid)
+    setStartMethods[params.startPositionConstraint](grid)
+  } catch (e) {
     if (retry < BuildRetryLimit) return buildMazeGrid(adjustParams(params), retry + 1)
     else throw Error(`could not build valid matrix`)
   }
 
-  // set stair
-  const deadEndPos = randomItemFromArray(grid.getDeadEnds())
-  grid.get(deadEndPos)!.type = 'stair'
-
-  // set start
-  console.log(params.startPositionConstraint)
-  const startPos = randomItemFromArray(grid.getCorridors())
-  const dir = grid.getCorridorDir(startPos)!
-  grid.get(startPos)!.start = { direction: getTurnedDirection(fireByRate(0.5) ? 'right' : 'left', dir) }
-
   return grid
-}
-
-const _buildMazeGrid = ({ size, fillRate, connRate }: BuildMazeGridParams): MazeGrid => {
-  const grid = new MazeGrid(size, size)
-  seedCells(grid, fillRate)
-  connectCells(grid, connRate)
-  return grid
-}
-
-const isValidMazeLevel = (grid: MazeGrid) => {
-  const deadEnds = grid.getDeadEnds()
-  if (!deadEnds.length) return false
-  const corridorNodes = grid.getCorridors()
-  if (!corridorNodes.length) return false
-  return true
 }
 
 const adjustParams = (params: BuildMazeGridParams): BuildMazeGridParams => {
   return {
     ...params,
     size: params.size + 1,
-    fillRate: params.fillRate * 1.05,
   }
 }
