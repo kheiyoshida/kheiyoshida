df3735cc891df521012f1f672cd71d09cb285427
kheiyoshida <kheiyoshida@gmail.com>
2026-01-12 09:30:49 +1100

    feat: adjust music alignment

--- pjs/maze/src/integration/query/music/index.ts
+++ pjs/maze/src/integration/query/music/index.ts
@@ -14,14 +14,14 @@ export const getMusicCommands = (): MusicCommand => ({
 })
 
 export const calcMusicAlignment = (sanity: number): MusicRange => {
-  if (sanity >= 2700) return 9
-  if (sanity >= 2400) return 8
-  if (sanity >= 2100) return 7
-  if (sanity >= 1800) return 6
-  if (sanity >= 1500) return 5
-  if (sanity >= 1200) return 4
-  if (sanity >= 900) return 3
-  if (sanity >= 600) return 2
+  if (sanity >= 2700) return 7
+  if (sanity >= 2400) return 6
+  if (sanity >= 2100) return 5
+  if (sanity >= 1800) return 4
+  if (sanity >= 1500) return 3
+  if (sanity >= 1200) return 2
+  if (sanity >= 900) return 2
+  if (sanity >= 600) return 1
   if (sanity >= 300) return 1
   return 1
 }
